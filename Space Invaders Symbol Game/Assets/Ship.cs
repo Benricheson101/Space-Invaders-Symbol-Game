c3fe44867c353e3047693ffa81f4b9c58be8e5b7
Benricheson101 <36977340+Benricheson101@users.noreply.github.com>
2018-06-23 10:15:38 -0400

    Moving Script
    Added: Moving Script

---
+++ Space Invaders Symbol Game/Assets/Ship.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    public float speed = 0.2f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("a"))
+        {
+            transform.Rotate(0, 0, 3);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Rotate(0, 0, -3);
+        }
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(Vector2.up * speed);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(-Vector2.up * speed);
+        }
+        if (Input.GetKey("left"))
+        {
+            transform.Rotate(0, 0, 3);
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.Rotate(0, 0, -3);
+        }
+        if (Input.GetKey("up"))
+        {
+            transform.Translate(Vector2.up * speed);
+        }
+        if (Input.GetKey("down"))
+        {
+            transform.Translate(-Vector2.up * speed);
+        }
+        if (Input.GetKey("left"))
+        {
+            transform.Rotate(0, 0, 3);
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.Rotate(0, 0, -3);
+        }
+        if (Input.GetKey("up"))
+        {
+            transform.Translate(Vector2.up * speed);
+        }
+        if (Input.GetKey("down"))
+        {
+            transform.Translate(-Vector2.up * speed);
+        }
+    }
+}
