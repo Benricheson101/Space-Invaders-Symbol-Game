1fe9215108e1afa6f0d164f04a817bf007fd6bd4
kmikerosen901 <40520931+kmikerosen901@users.noreply.github.com>
2018-06-25 14:41:11 -0400

    altered script 6/25/18 2:40
    altered the now-deleted script slightly

--- Space Invaders Symbol Game/Assets/_Alien.cs
+++ Space Invaders Symbol Game/Assets/_Alien.cs
@@ -13,7 +13,7 @@ public class _Alien : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (col.gameobject)
 	}
     private void OnTriggerEnter2D(Collider2D collision)
     {
