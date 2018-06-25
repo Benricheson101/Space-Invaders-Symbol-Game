3695caf27a83ae29c89b0e30dbb1bfc56b896277
CabooseNomerson <bshs14bcarl@gmail.com>
2018-06-25 13:21:21 -0400

    Spawner Fixes

--- Space Invaders Symbol Game/Assets/AlienSpawner.cs
+++ Space Invaders Symbol Game/Assets/AlienSpawner.cs
@@ -19,7 +19,7 @@ public class AlienSpawner : MonoBehaviour {
 	void Update () {
 		if(Time.time-interval>=LastAlien)
         {
-            transform.position = new Vector2( 0, Random.Range(ScreenLeft, ScreenRight));
+            transform.position = new Vector2( 11, Random.Range(ScreenLeft, ScreenRight));
         int RandomAlineIndex = Random.Range(0, 4);
             Instantiate(Alien[RandomAlineIndex], transform.position, Quaternion.identity);
             LastAlien = Time.time;
